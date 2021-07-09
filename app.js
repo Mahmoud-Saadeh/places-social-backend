e16a69753a0b119d9f7774f797f11ab8eb0ef210
Mahmoud-Saadeh <mahmoud.saadeh998@gmail.com>
2021-07-09 23:08:21 +0300

    cors4

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
-z;
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
