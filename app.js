646338c80be00ddbec198acf76e82ef2fe2ef423
Mahmoud-Saadeh <mahmoud.saadeh998@gmail.com>
2021-07-09 22:31:56 +0300

    cors

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
-const mongoose = require("mongoose");
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
 
-const placesRoutes = require("./routes/places-routes");
-const HttpError = require("./models/http-error");
-const usersRoutes = require("./routes/users-routes");
-const cors = require("cors");
+const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
+const usersRoutes = require('./routes/users-routes');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-
 app.use(cors());
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader(
@@ -23,11 +23,11 @@ app.use("/uploads/images", express.static(path.join("uploads", "images")));
 //   next();
 // });
 
-app.use("/api/places", placesRoutes);
-app.use("/api/users", usersRoutes);
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
+  const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
@@ -42,7 +42,7 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose.connect(
